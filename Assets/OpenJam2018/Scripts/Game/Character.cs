d77cf016944e1f719220c7bae77da04167a5dd6f
johnsoncodehk <johnsoncodehk@gmail.com>
2018-10-07 07:17:01 +0800

    Add Character: Swordsman

---
+++ Assets/OpenJam2018/Scripts/Game/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenJam2018
+{
+    public class Character : MonoBehaviour
+    {
+
+        public float moveRaw = 0;
+
+        CharacterController2D m_Controller;
+        Animator m_Animator;
+
+        void Awake()
+        {
+            m_Controller = GetComponent<CharacterController2D>();
+            m_Animator = GetComponent<Animator>();
+        }
+        void FixedUpdate()
+        {
+            m_Controller.Move(moveRaw * 40 * Time.fixedDeltaTime, false, false);
+        }
+        void Update()
+        {
+            m_Animator.SetFloat("Move Raw", moveRaw);
+        }
+
+        public virtual void LookAt(Vector2 position)
+        {
+
+        }
+        public virtual void Attack()
+        {
+            m_Animator.SetTrigger("Attack");
+        }
+    }
+}
