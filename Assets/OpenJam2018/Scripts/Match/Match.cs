beee64de1a07210e1a2c78ed811c9fe27bd83d98
johnsoncodehk <johnsoncodehk@gmail.com>
2018-10-13 19:03:17 +0800

    Cross-regional connection

--- Assets/OpenJam2018/Scripts/Match/Match.cs
+++ Assets/OpenJam2018/Scripts/Match/Match.cs
@@ -12,6 +12,7 @@ namespace OpenJam2018
 
         public GameObject searchingGame, noGameFound, creatingGame, gameCreated, gameCreateFailed, gameFound, joiningGame, gameJoinFailed, gameJoined, notSupport;
         public Text gameInfo;
+        public Dropdown serverDropdown;
 
         MatchInfoSnapshot m_MatchInfoSnapshot;
 
@@ -35,25 +36,45 @@ namespace OpenJam2018
         }
         public void SearchGame()
         {
+            SearchGame("eu1-mm.unet.unity3d.com", "Europe", () =>
+            {
+                SearchGame("us1-mm.unet.unity3d.com", "United States", () =>
+                {
+                    SearchGame("ap1-mm.unet.unity3d.com", "Singapore", () =>
+                    { });
+                });
+            });
+        }
+        void SearchGame(string url, string name, System.Action onFail)
+        {
             ShowState(searchingGame);
 
+            NetworkManagerHandler.instance.matchHost = url;
             NetworkManagerHandler.instance.StartMatchMaker();
             NetworkManagerHandler.instance.matchMaker.ListMatches(0, 1, "", true, 0, 0, (bool success, string extendedInfo, List<MatchInfoSnapshot> matches) =>
             {
                 if (matches.Count == 0)
                 {
                     ShowState(noGameFound);
+                    onFail();
                     return;
                 }
                 m_MatchInfoSnapshot = matches[0];
                 ShowState(gameFound);
-                gameInfo.text = "size: " + m_MatchInfoSnapshot.currentSize + "/" + m_MatchInfoSnapshot.maxSize;
+                gameInfo.text = "size: " + m_MatchInfoSnapshot.currentSize + "/" + m_MatchInfoSnapshot.maxSize + " (" + name + ")";
             });
         }
         public void CreateGame()
         {
             ShowState(creatingGame);
 
+            switch (serverDropdown.value)
+            {
+                case 0: NetworkManagerHandler.instance.matchHost = "mm.unet.unity3d.com"; break;
+                case 1: NetworkManagerHandler.instance.matchHost = "eu1-mm.unet.unity3d.com"; break;
+                case 2: NetworkManagerHandler.instance.matchHost = "us1-mm.unet.unity3d.com"; break;
+                case 3: NetworkManagerHandler.instance.matchHost = "ap1-mm.unet.unity3d.com"; break;
+            }
             NetworkManagerHandler.instance.StartMatchMaker();
             NetworkManagerHandler.instance.matchMaker.CreateMatch(name, 20, true, "", "", "", 0, 0, (bool success, string extendedInfo, MatchInfo matchInfo) =>
             {
@@ -72,7 +93,6 @@ namespace OpenJam2018
         {
             ShowState(joiningGame);
 
-            NetworkManagerHandler.instance.StartMatchMaker();
             NetworkManagerHandler.instance.matchMaker.JoinMatch(m_MatchInfoSnapshot.networkId, "", "", "", 0, 0, (bool success, string extendedInfo, MatchInfo matchInfo) =>
             {
                 if (!success)
